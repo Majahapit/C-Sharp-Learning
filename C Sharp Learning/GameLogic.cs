1ee087c9f68ef9881f81aa65f2cefbf2c1c2e9cf
Majahapit <121141464+Majahapit@users.noreply.github.com>
2026-02-11 17:30:37 -0600

    Added GameLogic.cs

---
+++ C Sharp Learning/GameLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Learning
+{
+    internal class GameLogic
+    {
+        private string Guess { get; set; }
+
+    }
+}
